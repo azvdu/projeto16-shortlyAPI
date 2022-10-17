528f0d3666408af37f36c370c3a88af4360a9910
Gabriel <gabriel.liradefarias2@gmail.com>
2022-10-17 14:12:38 -0300

    get route /urls/:id

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -10,11 +10,7 @@ export async function shortenUrl(req, res){
 
     try {
         const result = await db.query(`SELECT * FROM tokens WHERE token = $1`, [token]);
-        console.log(result.rows[0].userId)
-
-        if(!result){
-            return res.status(401).send("TOKEN INVÁLIDO!")
-        }
+ 
         await db.query(`
             INSERT INTO links (link, "linkShort", "userId", visits)
             VALUES ($1, $2, $3, $4)`, [link, linkShort, result.rows[0].userId, 0])
@@ -22,5 +18,21 @@ export async function shortenUrl(req, res){
     } catch (error) {
         console.log(error);
         return res.sendStatus(500);
+    }
+}
+
+export async function getUrlId(req, res){
+    const { id } = req.params;
+
+    try {
+        const result = (await db.query(`
+        SELECT id, "linkShort", link FROM links WHERE id = $1`, [id])).rows[0];
+        if(!result){
+            return res.sendStatus(404);
+        }
+        return res.status(200).send(result);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500)
     }
 }
--- src/routers/urlsRouters.js
+++ src/routers/urlsRouters.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 
 import { validateUrl } from "../middlewares/urlsMiddlewares.js";
 import { shortenUrl } from "../controllers/urlsController.js";
+import { getUrlId } from "../controllers/urlsController.js";
 
 const urlRouter = Router();
 urlRouter.post("/urls/shorten", validateUrl, shortenUrl);
+urlRouter.get("/urls/:id", getUrlId);
 
 
 export default urlRouter;

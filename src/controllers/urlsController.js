688f34ee0daf81b925a326f6e9b7bbdfdf8578dd
Gabriel <gabriel.liradefarias2@gmail.com>
2022-10-17 15:17:45 -0300

    get route /urls/open/:linkShort

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -5,8 +5,9 @@ import db from "../config/db.js";
 export async function shortenUrl(req, res){
     const { authorization } = req.headers;
     const token = authorization?.replace("Bearer ", "");
-    const link = req.body;
+    const link = req.body.url;
     const linkShort = nanoid(8);
+    console.log(link)
 
     try {
         const result = await db.query(`SELECT * FROM tokens WHERE token = $1`, [token]);
@@ -26,11 +27,29 @@ export async function getUrlId(req, res){
 
     try {
         const result = (await db.query(`
-        SELECT id, "linkShort", link FROM links WHERE id = $1`, [id])).rows[0];
+            SELECT id, "linkShort", link FROM links WHERE id = $1`, [id])).rows[0];
         if(!result){
             return res.sendStatus(404);
         }
         return res.status(200).send(result);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500)
+    }
+}
+
+export async function openShortUrl(req, res){
+    const { linkShort } = req.params;
+    try {
+        const result = (await db.query(`
+            SELECT * FROM links WHERE  "linkShort" = $1`, [linkShort])).rows[0];
+        const newVisits = Number(result.visits) + 1;
+        if(!result.link){
+            return res.sendStatus(404)
+        }
+        await db.query(`
+            UPDATE links SET visits=${newVisits} WHERE "linkShort" = $1`, [linkShort])
+        return res.redirect(result.link, 302)
     } catch (error) {
         console.log(error);
         return res.sendStatus(500)
--- src/routers/urlsRouters.js
+++ src/routers/urlsRouters.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
 import { validateUrl } from "../middlewares/urlsMiddlewares.js";
-import { shortenUrl } from "../controllers/urlsController.js";
-import { getUrlId } from "../controllers/urlsController.js";
+import { shortenUrl, getUrlId, openShortUrl } from "../controllers/urlsController.js";
 
 const urlRouter = Router();
 urlRouter.post("/urls/shorten", validateUrl, shortenUrl);
 urlRouter.get("/urls/:id", getUrlId);
+urlRouter.get("/urls/open/:linkShort", openShortUrl);
+
 
 
 export default urlRouter;
